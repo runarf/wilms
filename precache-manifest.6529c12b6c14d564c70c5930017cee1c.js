4da972d13db7ec8037506a7e21d6a9086f86f1aa
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-19 14:34:20 +0000

    Deploy runarf/wilms to github.com/runarf/wilms.git:gh-pages

---
+++ precache-manifest.6529c12b6c14d564c70c5930017cee1c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3053208c1e098edf4dc9e51f4b3f6cac",
+    "url": "/wilms/index.html"
+  },
+  {
+    "revision": "1cebca405d10c3d68194",
+    "url": "/wilms/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0c78e0f0994ab7b20ea2",
+    "url": "/wilms/static/js/2.7b47abc6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/wilms/static/js/2.7b47abc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cebca405d10c3d68194",
+    "url": "/wilms/static/js/main.578855aa.chunk.js"
+  },
+  {
+    "revision": "6ff460e890849daa755b",
+    "url": "/wilms/static/js/runtime-main.1eafdd4b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/wilms/static/media/logo.5d5d9eef.svg"
+  }
+]);
